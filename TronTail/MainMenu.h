c6d27c3753a58d22dff7ae3739a556630c133867
Landon Trevisani <landon.trevisani@gmail.com>
2025-04-09 20:33:49 -0400

    Fixed tiny window and resolution upon open

--- TronTail/MainMenu.h
+++ TronTail/MainMenu.h
@@ -7,26 +7,25 @@
 using namespace std;
 using namespace sf;
 
-// Enum for game states
+// enum to represent different states of the game
 enum GameState { MainMenuState, PlayingState, AboutState };
 
 class MainMenu {
 public:
-    MainMenu(float width, float height);
-    void MoveUp();
-    void MoveDown();
-    int MainMenuPressed();
-    void draw(RenderWindow& window);
-    ~MainMenu();
+    MainMenu(float width, float height); // constructor 
+    void MoveUp();                       // function to navigate menu upward
+    void MoveDown();                     // function to navigate menu downward
+    int MainMenuPressed();               // returns the currently selected menu item
+    void draw(RenderWindow& window);     // draws the menu on the window
+    ~MainMenu();                         // destructor
 
 private:
-    Text mainMenu[3]; // Menu: Play, About, Exit
-    Font font;
-    int MainMenuSelected;
-    Clock cooldownClock;
-
-    Texture backgroundTexture;
-    Sprite backgroundSprite;
+    Text mainMenu[3];          // Array to store menu options: Play, About, Exit
+    Font font;                 // Font used for the menu text
+    int MainMenuSelected;      // Index of the currently selected menu option
+    Clock cooldownClock;       // Clock to handle input cooldown for smooth navigation
+    Texture backgroundTexture; // Background texture for the menu
+    Sprite backgroundSprite;   // Sprite to render the background texture
 
     void setupMenuItem(Text& menuItem, const string& text, float centerX, float startingY, int index);
 };
